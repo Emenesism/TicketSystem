9e1cb8f01459f1ed5bc8066ae48f4e07f7172b3c
emenesism <mhdnris@gmail.com>
2026-05-13 18:34:53 +0330

    -

--- src/TicketSystem.Infrastructure/Persistance/Migrations/20260513145523_fkMig.Designer.cs
+++ src/TicketSystem.Infrastructure/Persistance/Migrations/20260513145523_fkMig.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using TicketSystem.Infrastructure.Persistance.Configuration;
@@ -11,9 +12,11 @@ using TicketSystem.Infrastructure.Persistance.Configuration;
 namespace TicketSystem.Infrastructure.Persistance.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260513145523_fkMig")]
+    partial class fkMig
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/TicketSystem.Infrastructure/Persistance/Migrations/20260513145523_fkMig.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TicketSystem.Infrastructure.Persistance.Migrations
+{
+    /// <inheritdoc />
+    public partial class fkMig : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "IpAddress",
+                table: "Sessions",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastUsageAt",
+                table: "Sessions",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserAgent",
+                table: "Sessions",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IpAddress",
+                table: "Sessions");
+
+            migrationBuilder.DropColumn(
+                name: "LastUsageAt",
+                table: "Sessions");
+
+            migrationBuilder.DropColumn(
+                name: "UserAgent",
+                table: "Sessions");
+        }
+    }
+}
